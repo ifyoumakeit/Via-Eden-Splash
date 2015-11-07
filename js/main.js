70b86e9f43c69d380101434db3389d8e4010b7c8
Dave Garwacke <admin+git@ifyoumakeit.com>
2015-11-07 15:06:57 -0500

    Form Submission and Background Video
    - Update background video
    - Add Loader

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 
+$('form').on('submit', function(evt){
+  evt.preventDefault();
+
+  success = function(){
+    $('.form').addClass('-fade-out')
+    $('.main__title').html('Thank you')
+  }
+
+  var email = $('#entry_1656576003').val();
+  if (email !== "" && validateEmail(email)) {
+    $('.loader').show()
+    $('.form__submit').hide()
+    $.ajax({
+      url: "https://docs.google.com/a/ifyoumakeit.com/forms/d/1m2gfIHsJdjFfWCbQmssWF8HRdKZdT8XiJtp57ROVtpo/formResponse",
+      data: {"entry.1656576003" : email},
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: success,
+        200: success
+      }
+    });
+  } else {
+      $('.field').addClass('-error')
+      $('.error-msg').show().html('Please enter a valid email address.')
+  }
+});
